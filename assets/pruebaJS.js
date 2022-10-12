e22f597c0bf7b2d98b7e36678cc01e93434c4c9c
Sofía Gómez Kusa <sofiagomezk@gmail.com>
2022-10-11 22:48:52 -0300

    Segunda Pre Entrega

--- assets/pruebaJS.js
+++ assets/pruebaJS.js
@@ -1,8 +1,10 @@
-
 let monto_minimo = 1000;
-let monto = prompt("¡Hola! Ingresa el monto que te gustaria financiar.");
+let cliente = prompt("¡Hola! Ingresa tu nombre")
+let monto = prompt(" Ingresa el monto que te gustaria financiar.");
 let cuotas = prompt("Ingresa en cuantas cuotas te gustaria pagarlo (3,6,12)");
 let calculo = monto * interes();
+let clientes_rechazados = ["Juan", "Raul"];
+
 
 
 function interes() {
@@ -13,16 +15,24 @@ function interes() {
 };
 
 
+function listadeclientesrechazados() {
+    console.log (clientes_pobres)
+    if (monto > monto_minimo) {
+        clientes_rechazados.push(cliente)
+     }
+};
+
+
 
 function financiacion() {
     if (monto >= monto_minimo) {
         return alert("El valor a devolver es de" + " " + calculo + " " + "en" + " " + cuotas + " " + "de" + " " + calculo / cuotas)
+    } else {
+        return alert("Lo sentimos, el monto que ingresaste no cumple con el valor minimo de financiación")
     }
-
-    else {
-        return alert("Lo sentimos, el monto que ingresaste no cumple con el valor minimo de financiación");
-    }
-}
+};
 
 financiacion();
 
+
+listadeclientesrechazados();
